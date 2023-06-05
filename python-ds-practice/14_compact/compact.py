3c9837a153dd645e66124e266fb141d372dd613c
Jeremy Romano <jar315@pitt.edu>
2023-06-04 23:51:04 -0400

    Finished all except fs exercises

--- python-ds-practice/14_compact/compact.py
+++ python-ds-practice/14_compact/compact.py
@@ -4,8 +4,9 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
-    copy = lst.copy()
-    for el in copy:
-        if not el or len(el) == 0:
-            copy.remove(el)
-    return copy
+    # copy = lst.copy()
+    # for el in copy:
+    #     if bool(el) == False:    #Why does this not work? (It doesn't remove the empty list or tuple)
+    #         copy.remove(el)
+    # return copy
+    return [el for el in lst if el]
